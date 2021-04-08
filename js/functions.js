c4293436c51d787707b8c1cb682477a0013de21a
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-04-08 21:44:54 +0200

    Improve js file

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!'){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
